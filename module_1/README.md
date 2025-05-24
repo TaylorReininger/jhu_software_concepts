32ab8fee7ccac3f9420d1fbdf83682478b9c1433
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-05-24 11:53:01 -0400

    Prototyping initial Flask concepts to gain familiarity with syntax. Added sources to README.

--- module_1/README.md
+++ module_1/README.md
@@ -2,3 +2,12 @@
 
 ## Assignment
 
+
+
+
+## Resources
+
+I followed along with a lot of the course notes from the lecture
+
+I found the [Flask quickstart](https://flask.palletsprojects.com/en/stable/quickstart/) guide very helpful for reading about the decorators and the ```__name__``` field at instantiation. 
+
--- module_1/main.py
+++ module_1/main.py
@@ -1,4 +1,20 @@
 
+from flask import Flask
 
 
 
+# Create Web Server Gateway Interface (WSGI) application by instantiating a 
+# Flask object
+web_app = Flask(__name__)
+
+
+# Use the route() decorator to define which URL triggers the function.
+@web_app.route("/")
+# Custom function to display web content (can use HTML syntax here if desired)
+def hello_world():
+    return "Hello, World! My name is Taylor"
+
+
+# Run the webapp with appropriate IP and port values
+if __name__ == '__main__':
+    web_app.run(host='0.0.0.0', port=8080)    
