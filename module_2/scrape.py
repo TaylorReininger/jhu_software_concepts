06c76687fcdc87084a2f5bd6b0e4c2009ad5c64d
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-05-31 11:09:28 -0400

    Better comments

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -117,7 +117,6 @@ class Scrape:
             pickle.dump(self.data, file)
 
 
-
     def scrape_grad_cafe(self, num_apps: int, save_path: str) -> None:
         
         # Ensure that we don't overwrite existing pkl files
@@ -148,11 +147,14 @@ class Scrape:
 
 if __name__ == "__main__":
     
+    # Create scrape object
     s = Scrape()
+
+    # Call the method to scrape with the desired input parameters
     path_pkl = 'test_25.pkl'
     s.scrape_grad_cafe(25, path_pkl)
 
-
+    # Check that the file is created and is readable
     if os.path.exists(path_pkl):
 
         # Load in the data from the pickle file
@@ -160,82 +162,3 @@ if __name__ == "__main__":
             stuff = pickle.load(file)
 
         print(len(stuff))
-
-
-"""
-
-# Save location of raw HTML
-path_html = 'html.pkl'
-
-# If it's not already saved off, pull the data from the web
-# otherwise, load in the saved version
-if os.path.exists(path_html):
-
-    print('LOADING')
-    # Load in the data from the pickle file
-    with open(path_html, 'rb') as file:
-        html = pickle.load(file)
-
-else:
-
-    print('SCRAPING')
-
-    # Create a URL object
-    page = urlopen(url)
-
-    # Read the web page and convert it to html
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-
-    # Write the HTML to file for future use to reduce page hits
-    with open(path_html, 'wb') as file:
-        pickle.dump(html, file)
-
-
-# Use beautiful soup to parse the HTML
-soup = BeautifulSoup(html, "html.parser")
-
-# From the survey page, load in the whole table
-table_body = soup.find('tbody')
-
-
-# Extract the data of interest from the table
-rows = table_body.find_all('tr')
-
-# Initialize some lists for extracting the tabular data
-this_page_data = []
-curr_line = []
-
-# Parese each row in the table
-for row in rows:
-
-    # This finds an individual row from the table and turns it into a list
-    this_row = row.find_all('td')
-    this_row = [cell.text.strip() for cell in this_row]
-
-    # This takes care of blank lines that sometimes make it into the table
-    if this_row[0] == '':
-        continue
-
-    # If this row is not a header, keep building onto the previous list
-    if row.attrs and 'class' in row.attrs.keys():
-        curr_line.extend(this_row)
-
-    # If this row is a header, store the previously built row list and start a new one
-    else:
-        this_page_data.append(curr_line)
-        curr_line = this_row
-        
-# Make sure to catch the last row we were building
-this_page_data.append(curr_line)
-
-
-# Sanity check:
-for r in this_page_data:
-    print('-----------------------------------------------------')
-    print(r)
-    print(len(r))
-
-
-"""
-
