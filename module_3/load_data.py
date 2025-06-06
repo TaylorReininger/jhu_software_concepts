a132f78c845c534f7fff1350502b882d2ba5d40b
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-06 15:42:19 -0400

    Added load_data file with notes about what should go in there.

---
+++ module_3/load_data.py
@@ -0,0 +1,25 @@
+
+
+"""
+Notes:
+
+- All the data shall go into a table named "applicants"
+- The table should have these fields:
+
+| **Column Name** | **Data Type** | **Description** |
+| --- | --- | --- |
+| p_id | integer | Unique identifier |
+| program | text | University and Department |
+| comments | text | Comments |
+| date_added | date | Date Added |
+| url | text | Link to Post on Grad Café |
+| status | text | Admission Status |
+| term | text | Start Term |
+| us_or_international | text | Student nationality |
+| gpa | float | Student GPA |
+| gre | float | Student GRE Quant |
+| gre_v | float | Student GRE Verbal |
+| gre_aw | float | Student Average Writing |
+| degree | text | Student Program Degree Type |
+
+"""
