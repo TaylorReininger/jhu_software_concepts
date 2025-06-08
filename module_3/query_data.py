776dc56c08dacd5af559eaeb14bb3c1096c73be4
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-07 22:36:05 -0400

    Added draft query for all 7 questions.

--- module_3/query_data.py
+++ module_3/query_data.py
@@ -2,111 +2,196 @@ import psycopg
 
 
 
+class QueryData:
 
+    def __init__(self):
 
+        self.name_db = 'module3'
+        self.name_table = 'applications'
+        self.current_term = 'Spring 2025'
 
+        self.conn = psycopg.connect(dbname=self.name_db, user="postgres")
+        self.cur = self.conn.cursor()
 
-
-
-
-
-
-name_db = 'module3'
-name_table = 'applications'
-
-
-
-
-# Connect to a postgresql server (must follow steps from above)
-with psycopg.connect(dbname=name_db, user="postgres") as conn:
-
-
-    # Open a cursor to perform database operations
-    with conn.cursor() as cur:
-    
         # Display all the content from the table at this stage
-        cur.execute("""
+        self.cur.execute("""
             select * from %s;
-            """%(name_table))
+            """%(self.name_table))
 
         # Print outputs
-        table_content = cur.fetchall()
-        #print("The table has this content in it: %s"%(table_content))
+        table_content = self.cur.fetchall()
+        print("The table has this content in it: %s"%(table_content))
 
 
-        # Problem 1: How many entries do you have in your database who have applied for Fall 2024?
+
+    def q1(self):
+
+        # Problem 1: How many entries do you have in your database who have applied for Spring 2025?
         text_query = """SELECT *
             FROM %s
             WHERE %s = '%s';
-            """%(name_table, 'term', 'Spring 2025')
+            """%(self.name_table, 'term', self.current_term)
 
-        cur.execute(text_query)
+        self.cur.execute(text_query)
         # Print outputs
-        output = cur.fetchall()
-        #print("Here's your output: %s"%(len(output)))
+        output = self.cur.fetchall()
+        print("Here's your output: %s"%(len(output)))
 
+
+    def q2(self):
 
         # Problem 2: What percentage of entries are from international students (not American or Other) (to two decimal places)?
         text_query = """SELECT 
             %s
             FROM %s;
-            """%('us_or_international', name_table)
+            """%('us_or_international', self.name_table)
 
-        cur.execute(text_query)
+        self.cur.execute(text_query)
         # Print outputs
-        output = cur.fetchall()
+        output = self.cur.fetchall()
         #print("Here's your output: %s"%(output))
 
+
+    def q3(self):
 
         # Problem 3: What is the average GPA, GRE, GRE V, GRE AW of applicants who provide these metrics?
         text_query = """SELECT 
             %s
             FROM %s
             WHERE %s != '-1';
-            """%('gpa', name_table, 'gpa')
+            """%('gpa', self.name_table, 'gpa')
 
-        cur.execute(text_query)
+        self.cur.execute(text_query)
         # Print outputs
-        gpa = cur.fetchall()
+        gpa = self.cur.fetchall()
         print("Here's your output: %s"%(gpa))
 
         text_query = """SELECT 
             %s
             FROM %s
             WHERE %s != '-1';
-            """%('gre', name_table, 'gre')
+            """%('gre', self.name_table, 'gre')
 
-        cur.execute(text_query)
+        self.cur.execute(text_query)
         # Print outputs
-        gre = cur.fetchall()
+        gre = self.cur.fetchall()
         print("Here's your output: %s"%(gre))
 
         text_query = """SELECT 
             %s
             FROM %s
             WHERE %s != '-1';
-            """%('gre_v', name_table, 'gre_v')
+            """%('gre_v', self.name_table, 'gre_v')
 
-        cur.execute(text_query)
+        self.cur.execute(text_query)
         # Print outputs
-        gre_v = cur.fetchall()
+        gre_v = self.cur.fetchall()
         print("Here's your output: %s"%(gre_v))
 
         text_query = """SELECT 
             %s
             FROM %s
             WHERE %s != '-1';
-            """%('gre_aw', name_table, 'gre_aw')
+            """%('gre_aw', self.name_table, 'gre_aw')
 
-        cur.execute(text_query)
+        self.cur.execute(text_query)
         # Print outputs
-        gre_aw = cur.fetchall()
+        gre_aw = self.cur.fetchall()
         print("Here's your output: %s"%(gre_aw))
 
 
 
+    def q4(self):
+
+        # Problem 4: What is their average GPA of American students in Spring 2025?
+        text_query = """SELECT 
+            %s
+            FROM %s
+            WHERE %s = 'American' 
+            AND %s = '%s';
+            """%('gpa', self.name_table, 'us_or_international', 'term', self.current_term)
+
+        self.cur.execute(text_query)
+        # Print outputs
+        recent_us_gpas = self.cur.fetchall()
+        print("Here's your output: %s"%(recent_us_gpas))
 
 
+    def q5(self):
+
+        # Problem 5: What percent of entries for Spring 2025 are Acceptances (to two decimal places)?
+        text_query = """SELECT 
+            %s
+            FROM %s
+            WHERE %s = '%s';
+            """%('status', self.name_table, 'term', self.current_term)
+
+        self.cur.execute(text_query)
+        # Print outputs
+        recent_acceptances = self.cur.fetchall()
+        print("Here's your output: %s"%(recent_acceptances))
+
+
+    def q6(self):
+
+        # Problem 6: What is the average GPA of applicants who applied for Spring 2025 who are Acceptances?
+        text_query = """SELECT 
+            %s
+            FROM %s
+            WHERE %s = '%s'
+            AND %s = 'Accepted';
+            """%('gpa', self.name_table, 'term', self.current_term, 'status')
+
+        self.cur.execute(text_query)
+        # Print outputs
+        recent_acceptance_gpa = self.cur.fetchall()
+        print("Here's your output: %s"%(recent_acceptance_gpa))
+
+
+
+    def q7(self):
+
+        # Problem 7: How many entries are from applicants who applied to JHU for a masters degrees in Computer Science?
+        text_query = """SELECT 
+            %s
+            FROM %s
+            WHERE %s = '%s'
+            AND %s = '%s';
+            """%('p_id', self.name_table, 'program', 'JHU', 'degree', 'Masters')
+
+        self.cur.execute(text_query)
+        # Print outputs
+        hopkins_ms_cs = self.cur.fetchall()
+        print("Here's your output: %s"%(hopkins_ms_cs))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # Create the query data object
+    qd = QueryData()
+
+    # Run all 7 queries
+    qd.q1()
+    qd.q2()
+    qd.q3()
+    qd.q4()
+    qd.q5()
+    qd.q6()
+    qd.q7()
 
 
 
