bedc8a47ffcdcf71f97063d8e780e570c0b9160b
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-09 21:49:51 -0400

    Added tests per the Test-Driven Development workflow.

--- module_4/tests/unit_tests/test_order.py
+++ module_4/tests/unit_tests/test_order.py
@@ -1,26 +1,43 @@
+import re
 
 import pytest
 
 from src.order import Order
+from src.pizza import Pizza
 
 
-
+@pytest.fixture
 @pytest.mark.order
 def test_init():
 
+    # Creat an Order object
     o = Order()
 
+    # Ensure the pizzas class member is of type "list"
+    assert isinstance(o.pizzas, list)
+
+    # Ensure that the cost class member is initialized at zero
+    assert o.cost == 0
+
+    # Ensure that the paid class member is initialized to "False"
+    assert o.paid == False
+    
+
+@pytest.fixture
+@pytest.mark.order
+def test_input_pizza():
     assert True
+
 
 
 @pytest.mark.order
 def test_str():
-    assert True
+
+    gpa = re.search(r'GPA \d+\.[0-9]+', row[6])
 
 
-@pytest.mark.order
-def test_input_pizza():
     assert True
+
 
 
 @pytest.mark.order
