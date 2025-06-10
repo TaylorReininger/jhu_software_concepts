7677beb4181638c4376a25afa8bcb4a4baa54205
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-09 21:18:13 -0400

    Created template of test_order methods.

---
+++ module_4/tests/test_order.py
@@ -0,0 +1,26 @@
+
+import pytest
+
+
+
+def __init__():
+    assert True
+
+
+def __str__():
+    assert True
+
+
+def input_pizza():
+    assert True
+
+
+def order_paid():
+    assert True
+
+
+
+
+
+
+
