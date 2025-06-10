716f594e3407e1583d17fb06f0550489cceb2cb1
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-09 21:32:07 -0400

    Moved tests into appropriate sub folders and added __init__ file to help with imports.

---
+++ module_4/tests/unit_tests/test_order.py
@@ -0,0 +1,35 @@
+
+import pytest
+
+from src.order import Order
+
+
+
+@pytest.mark.order
+def test_init():
+
+    o = Order()
+
+    assert True
+
+
+@pytest.mark.order
+def test_str():
+    assert True
+
+
+@pytest.mark.order
+def test_input_pizza():
+    assert True
+
+
+@pytest.mark.order
+def test_order_paid():
+    assert True
+
+
+
+
+
+
+
