d45895fcfd56bca16e12a8593895d24111b94b22
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-10 21:54:43 -0400

    Added function definitions to Order class.

--- module_4/src/order.py
+++ module_4/src/order.py
@@ -5,5 +5,18 @@
 class Order:
 
     def __init__(self):
+        pass
+    
+    
+    def __str__(self):
+        pass
+    
+    
+    def input_pizza(self, crust: str='thin', sauce: Union[str, List]='marinara', cheese: Union[str, None]=None, toppings: Union[str, List]=['pepperoni']) -> None:
 
+
+        pass
+
+
+    def order_paid(self):
         pass
