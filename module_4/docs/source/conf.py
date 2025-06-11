0fd3b0b3d0cfb7b8bde59cea5ac3e418eb4e85fe
Taylor Reininger <50842053+TaylorReininger@users.noreply.github.com>
2025-06-11 18:33:30 -0400

    Finished formatting and layout for Sphinx documentation.

--- module_4/docs/source/conf.py
+++ module_4/docs/source/conf.py
@@ -9,20 +9,29 @@
 # Import some libraries
 import os
 import sys
+import shutil
 
+# Some pathing
 sys.path.append(os.path.abspath("../.."))
 sys.path.append(os.path.abspath(".."))
 
+# Copy images into the Sphinx working folder so that MystParser can use
+# them in the README
+shutil.copytree("../../figs", "./figs", dirs_exist_ok=True)
 
+# Project meta data
 project = 'Module4'
 copyright = '2025, Taylor J. Reininger'
 author = 'Taylor J. Reininger'
 release = '0.0.1'
 
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'myst_parser']
+source_suffix = ['.rst', '.md']
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -34,3 +43,8 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+# Set a logo for the documentation
+html_logo = "../../figs/pizza_image.jpg"
+
--- module_4/docs/source/index.rst
+++ module_4/docs/source/index.rst
@@ -3,15 +3,23 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Module4 documentation
-=====================
+Welcome to my Module 4 Documentation!!!
+==========================================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
 
+.. include:: ../../README.md
+   :parser: myst_parser.sphinx_
+
+
+
+Overview
+=========
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 3
+   :numbered:
+   :caption: Modules
+
+   modules
+
 
